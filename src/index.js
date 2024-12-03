8550bffa219b2549416aa8e596f027a00971af4a
Hrushikesh Kadudula <hrushikesh.1804@gmail.com>
2024-12-03 14:05:01 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path:'./env'})
 
 import dotenv from "dotenv"
+import { app } from "./app.js";
 
 import connectDB from "./db/index.js";
 
@@ -9,7 +10,13 @@ dotenv.config({
 })
 
 
-connectDB()
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Sever is running at port:${process.env.PORT}`)
+    })
+}).catch((err)=>{
+    console.log("MONGODB connection failed !!!",err)
+})
 
 
 
